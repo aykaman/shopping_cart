ac4ba012f0136b666ced1b3baf517b0bff51ecf1
aykaman <aybeka@gmail.com>
2020-01-20 14:10:56 +0600

    add partials

--- index.php
+++ index.php
@@ -1,41 +1,7 @@
 <?php
   require_once "config.php";
+  include("./partials/header.php");
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Shopping cart</title>
-
-    <link
-      rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-      crossorigin="anonymous"
-    />
-    <link
-      rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-    />
-    <link rel="stylesheet" href="style.css" />
-  </head>
-  <body>
-    <nav
-      class="navbar navbar-dark bg-primary justify-content-between align-middle"
-    >
-      <a href="index.html" class="navbar-brand">Shopping cart</a>
-      <form class="form-inline text-white">
-        <span class="pr-2">Balance:</span>
-        <span class="badge badge-danger mr-3">$100.00</span>
-        <a href="myCart.html" class="btn btn-warning">
-          My cart <i class="fa fa-shopping-cart"></i>
-          <span class="cart-count badge badge-secondary">0</span>
-        </a>
-      </form>
-    </nav>
 
     <div class="container mt-3">
       <div class="row">
@@ -110,20 +76,6 @@
       </div>
     </div>
 
-    <script
-      src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-      integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-      integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-      integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-      crossorigin="anonymous"
-    ></script>
-  </body>
-</html>
+<?php
+  include("./partials/footer.php");
+?>
---
+++ partials/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Shopping cart</title>
+
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <nav
+      class="navbar navbar-dark bg-primary justify-content-between align-middle"
+    >
+      <a href="index.html" class="navbar-brand">Shopping cart</a>
+      <form class="form-inline text-white">
+        <span class="pr-2">Balance:</span>
+        <span class="badge badge-danger mr-3">$100.00</span>
+        <a href="myCart.html" class="btn btn-warning">
+          My cart <i class="fa fa-shopping-cart"></i>
+          <span class="cart-count badge badge-secondary">0</span>
+        </a>
+      </form>
+    </nav>
