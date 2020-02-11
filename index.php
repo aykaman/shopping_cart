f63b80cd86b1292d0fbcf0c0b6cb750bc1be48b5
aykaman <aybeka@gmail.com>
2020-02-11 10:35:21 +0600

    cart add

--- index.php
+++ index.php
@@ -15,27 +15,31 @@
         ?>
 
                 <div class="col-lg-3 text-center">
-                <div class="card h-100 bg-light">
-                    <img src="images/<?= $p['image'] ?>" />
-                    <div class="card-body">
-                    <div class="cart-title">
-                        <h4><?= $p['name'] ?></h4>
-                        <h5>$<?= $p['price'] ?></h5>
-                        <small class="text-muted"
-                        >&#9733; &#9733; &#9733; &#9733; &#9734;</small
-                        >
-                    </div>
-                    </div>
-                    <div class="card-footer">
-                    <button class="btn btn-primary">Add to cart</button>
-                    </div>
-                </div>
+                  <div class="card h-100 bg-light">
+                      <img src="images/<?= $p['image'] ?>" />
+                      <div class="card-body">
+                      <div class="cart-title">
+                          <h4><?= $p['name'] ?></h4>
+                          <h5>$<?= $p['price'] ?></h5>
+                          <small class="text-muted"
+                          >&#9733; &#9733; &#9733; &#9733; &#9734;</small
+                          >
+                      </div>
+                      </div>
+                      <div class="card-footer">
+                      <input type="button" class="btn btn-primary" onclick="cart.add(<?= $row['product_id'] ?>);" value="Add to cart">
+                      </div>
+                  </div>
                 </div>
         
         <?php
             }
           }
         ?>
+      </div>
+
+      <div class="row">
+        <div id="page-cart" class="ninja"></div>
       </div>
     </div>
 
